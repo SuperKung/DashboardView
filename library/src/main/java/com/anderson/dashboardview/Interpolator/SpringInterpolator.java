dc4ff89e9a73fcd231cbe693ef3b76c13526b30a
SuperKung <kung1020@outlook.com>
2016-07-02 22:43:25 +0800

    fix bug , SpringInterpolator support 4.4

--- library/src/main/java/com/anderson/dashboardview/Interpolator/SpringInterpolator.java
+++ library/src/main/java/com/anderson/dashboardview/Interpolator/SpringInterpolator.java
@@ -3,16 +3,18 @@ package com.anderson.dashboardview.Interpolator;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.view.animation.BaseInterpolator;
+import android.view.animation.Interpolator;
 
 /**
  * Created by User1 on 2016/6/16 0016.
  */
 
-@TargetApi(Build.VERSION_CODES.LOLLIPOP_MR1)
-public class SpringInterpolator extends BaseInterpolator {
+
+public class SpringInterpolator implements Interpolator {
     private final float mTension;
     public SpringInterpolator() {
         mTension = 0.4f;
+
     }
     public SpringInterpolator(float tension) {
         mTension = tension;
